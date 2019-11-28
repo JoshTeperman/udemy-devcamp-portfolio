719b6c14e4d035d9fec1ae27417a7b7aba9248cd
Josh Teperman <joshteperman@gmail.com>
2019-11-29 08:10:40 +1100

    edit & update portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,15 +1,17 @@
 class PortfoliosController < ApplicationController
+  before_action :set_item, only: [:delete, :show, :edit, :update]
+
   def index
     @portfolio_items = Portfolio.all
   end
 
   def new
-    @portfolio = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
 
   def create
-    @portfolio = Portfolio.new(portfolio_params)
-    if @portfolio.save
+    @portfolio_item = Portfolio.new(portfolio_params)
+    if @portfolio_item.save
       redirect_to portfolios_path
     else
       return 'Error'
@@ -17,12 +19,30 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Successfully updated'}
+      else
+        format.html { render :edit}
+      end
+    end
+  end
+
+  def delete
+  end
 
   private
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+  def set_item
+    @portfolio_item ||= Portfolio.find(params[:id])
   end
 end
