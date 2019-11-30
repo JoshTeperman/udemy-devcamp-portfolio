6b5356ce6aedfea805d7acb6a7963152a6700f38
Josh Teperman <joshteperman@gmail.com>
2019-11-30 22:54:07 +1100

    install friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
+  resources :portfolios, except: [:show]
 
-  get 'pages/about'
-
-  get 'pages/contact'
+  get '/portfolios/:id', to: 'portfolios#show'
+  get 'about', to: 'pages#about', as: 'about_page'
+  get 'contact', to: 'pages#contact', as: 'contact_page'
 
   resources :blogs
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
