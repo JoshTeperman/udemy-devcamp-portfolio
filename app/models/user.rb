fed132d7b5a793657f443f107835729d633f22fb
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-09 10:58:43 +1100

    first, middle, last_name methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  def first_name
+    name.split.first.capitalize
+  end
+
+  def middle_name
+    name.split[1..-2].map(&:capitalize).join(' ') if name.split.length > 2
+  end
+
+  def last_name
+    name.split.last.capitalize if name.split.length > 1
+  end
 end
