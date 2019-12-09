e70aa21474623c154e388442e8453f1e679a4c99
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-09 12:25:22 +1100

    session tracker for source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+
+  before_action :set_referrer
+
+  private
+
+  def set_referrer
+    session[:referrer] = params[:q] if params[:q]
+  end
 end
