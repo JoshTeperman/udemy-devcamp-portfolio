57e7df808114785eeb372f22f4166f298c413622
Josh Teperman <Josh.teperman@weployapp.com>
2019-12-10 20:22:02 +1100

    refactor with content_helper, content_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
-  def sample_helper
-    '<h1> This is a sample helper</h1>'
+  def login_helper
+    if current_user.is_a?(User)
+      link_to 'Logout', destroy_user_session_path, method: :delete
+    else
+      link_to 'Register', new_user_registration_path
+      link_to 'Login', new_user_session_path
+    end
+  end
+
+  def source_helper
+    greeting = "Welcome. Thanks for visiting us from #{session[:referrer]}"
+    content_tag(:h3, greeting, class: 'source-greeting') if session[:referrer]
   end
 end
